cc1e6bbd29e827ce9086d9ae3733331013193a04
karvaneg <m.lecarvennec@cbnsa.fr>
2025-12-10 22:40:56 +0100

    🎉 T1 : Initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col">
+      <div>PORTFOLIO KARVANEG</div>
+      {/* Section Hero */}
+      {/* Section À propos */}
+      {/* Section Skills */}
+      {/* Section Experience */}
+      {/* Section Projects */}
+      {/* Section Contact */}
+    </main>
+  );
+}
