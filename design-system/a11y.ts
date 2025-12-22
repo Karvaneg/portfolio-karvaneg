d73667d0a516a4f77bcb3b6d7fc8f0b60f688fba
karvaneg <m.lecarvennec@cbnsa.fr>
2025-12-22 21:19:00 +0100

    ♻️T23 : DESIGN SYSTEM > add ariaLabels

---
+++ design-system/a11y.ts
@@ -0,0 +1,5 @@
+export const ariaLabels = {
+  externalLink: (label: string) => `Ouvrir ${label} (nouvel onglet)`,
+  toggleTheme: (isDark: boolean) =>
+    isDark ? "Activer le mode clair" : "Activer le mode sombre",
+};
