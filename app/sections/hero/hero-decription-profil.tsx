00b56bfb0606986fd5192ad98de7de93c43051d2
karvaneg <m.lecarvennec@cbnsa.fr>
2025-12-27 22:42:29 +0100

    💄T27 : HERO > add vertival maker

--- app/sections/hero/hero-decription-profil.tsx
+++ app/sections/hero/hero-decription-profil.tsx
@@ -9,15 +9,24 @@ export function HeroDescriptionProfil() {
             <h1 className={cn("text-5xl md:text-7xl font-bold text-balance")}>{profileData.name}</h1>
             <p className={cn("text-xl md:text-2xl", dsTokens.color.textSecondary)}>{profileData.title}</p>
         </div>
-
-        <p className={cn("text-lg md:text-xl max-w-2xl text-pretty leading-relaxed", dsTokens.color.textSecondary)}>
-            {profileData.description.map((desc) => (
-                <span key={desc.line}>
-                    {desc.line}
-                    <br />
-                </span>
-            ))}
-        </p>
+        <div className={cn("relative pl-6")}>
+            {/* Barre verticale */}
+            <span
+                aria-hidden
+                className={cn(
+                "absolute left-0 top-1 bottom-1 w-[3px] rounded-full",
+                dsTokens.color.borderBg,
+                )}
+            />
+            <p className={cn("text-lg md:text-xl max-w-2xl text-pretty leading-relaxed", dsTokens.color.textSecondary)}>
+                {profileData.description.map((desc) => (
+                    <span key={desc.line}>
+                        {desc.line}
+                        <br />
+                    </span>
+                ))}
+            </p>
+        </div>
     </>
   )
 }
