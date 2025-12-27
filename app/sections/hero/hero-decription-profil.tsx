ca67a64900d1d6f9840abe3e54b84ba01c3a09ae
karvaneg <m.lecarvennec@cbnsa.fr>
2025-12-27 23:07:40 +0100

    ♻️T27 : HERO > Apply VerticalMarker component

--- app/sections/hero/hero-decription-profil.tsx
+++ app/sections/hero/hero-decription-profil.tsx
@@ -1,4 +1,5 @@
 import { profileData } from "@/app/lib/data/profile";
+import { VerticalMarker } from "@/components/ui-kit/vertical-maker";
 import { dsTokens } from "@/design-system/tokens";
 import { cn } from "@/lib/utils";
 
@@ -9,15 +10,8 @@ export function HeroDescriptionProfil() {
             <h1 className={cn("text-5xl md:text-7xl font-bold text-balance")}>{profileData.name}</h1>
             <p className={cn("text-xl md:text-2xl", dsTokens.color.textSecondary)}>{profileData.title}</p>
         </div>
-        <div className={cn("relative pl-6")}>
-            {/* Barre verticale */}
-            <span
-                aria-hidden
-                className={cn(
-                "absolute left-0 top-1 bottom-1 w-[3px] rounded-full",
-                dsTokens.color.borderBg,
-                )}
-            />
+        
+        <VerticalMarker>
             <p className={cn("text-lg md:text-xl max-w-2xl text-pretty leading-relaxed", dsTokens.color.textSecondary)}>
                 {profileData.description.map((desc) => (
                     <span key={desc.line}>
@@ -26,7 +20,7 @@ export function HeroDescriptionProfil() {
                     </span>
                 ))}
             </p>
-        </div>
+        </VerticalMarker>
     </>
   )
 }
