57e1a295f04bbcf38f4f0deb0f3c97f6c020232c
karvaneg <m.lecarvennec@cbnsa.fr>
2025-12-27 22:41:39 +0100

    💬T27 : HERO > Add line in description

--- app/lib/data/profile.ts
+++ app/lib/data/profile.ts
@@ -4,6 +4,7 @@ export const profileData = {
   description: [
     { line: "Le Clean code, même quand personne ne regarde."},
     { line: "Coeur de stack Symfony, React et PostgreSQL."},
+    { line: "Interfaces fiables, accessibles et pensées pour durer."},
   ],
   image: "/images/mlc-square-cyan.webp",
   imageAlt: "Photo de Marie Le Carvennec, développeuse web fullstack",
