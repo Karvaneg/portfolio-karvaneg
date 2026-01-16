0a48430d6f4bdee1c25521ee83ab2196b55e4405
karvaneg <m.lecarvennec@cbnsa.fr>
2026-01-16 20:21:22 +0100

    💬 : DATA > testimonials > modify apostrophe

--- app/lib/data/testimonials.ts
+++ app/lib/data/testimonials.ts
@@ -3,7 +3,7 @@ import type { TestimonialProps } from "@/types/ui-kit";
 export const testimonialsData: readonly TestimonialProps[] = [
   {
     id: "cbnsa-coralie-pradel",
-    text: "Marie, une collaboratrice compétente, investie, à l’écoute et ayant le goût des challenges professionnels qu’elle a su relever durant ses deux années au CBNSA. Avec un bel esprit d’équipe, elle a beaucoup apporté au Conservatoire et aux équipes. Chanceux seront ses futurs employeurs.",
+    text: "Marie, une collaboratrice compétente, investie, à l'écoute et ayant le goût des challenges professionnels qu'elle a su relever durant ses deux années au CBNSA. Avec un bel esprit d'équipe, elle a beaucoup apporté au Conservatoire et aux équipes. Chanceux seront ses futurs employeurs.",
     author: "Coralie Pradel",
     role: "Directrice générale des services",
     featured: true,
