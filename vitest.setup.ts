23e72c44a118b300498849e86b210e96d4127919
karvaneg <m.lecarvennec@cbnsa.fr>
2026-01-22 19:54:27 +0100

    🧪 F04: add Vitest config, setup, and globals types

---
+++ vitest.setup.ts
@@ -0,0 +1,21 @@
+import '@testing-library/jest-dom/vitest'
+import React from 'react'
+import { vi } from 'vitest'
+
+vi.mock('next/image', () => ({
+  default: ({
+    alt,
+    fill,
+    ...props
+  }: React.ImgHTMLAttributes<HTMLImageElement> & { fill?: boolean }) =>
+    React.createElement('img', { alt: alt ?? '', ...props }),
+}))
+
+vi.mock('next/link', () => ({
+  default: ({
+    href,
+    children,
+    ...props
+  }: React.AnchorHTMLAttributes<HTMLAnchorElement> & { href: string }) =>
+    React.createElement('a', { href, ...props }, children),
+}))
