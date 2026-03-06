712fcf7b570d49c35fcd024b5c822797ee29891e
karvaneg <m.lecarvennec@cbnsa.fr>
2026-03-06 15:51:41 +0100

    🩹[FIX] Ensures the contrast between foreground and background colors meets WCAG 2 AA minimum contrast ratio thresholds

--- app/sections/skills/skills-header.tsx
+++ app/sections/skills/skills-header.tsx
@@ -5,7 +5,7 @@ export function SkillsHeader() {
   return (
     <div className={cn('text-center mb-12')}>
       <SectionTitle className={cn('mb-4 text-balance')}>Compétences & Expertises</SectionTitle>
-      <p className={cn('text-muted-foreground max-w-2xl mx-auto text-pretty')}>
+      <p className={cn('text-foreground max-w-2xl mx-auto text-pretty')}>
         Un aperçu de mes compétences techniques et humaines acquises tout au long de mon parcours
       </p>
     </div>
